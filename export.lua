d179999339a8665b1ca357c4a4d2b5ddfdf1d8cd
Qwreey <qwreey75@gmail.com>
2023-04-14 00:03:53 +0900

    move types into export.lua

--- export.lua
+++ export.lua
@@ -1,4 +1,25 @@
+export type spring = {
+	Mass: number;
+	Damping: number;
+	Constant: number;
+	InitialOffset: number;
+	InitialVelocity: number;
+	ExternalForce: number;
 
-local types = require(script.types)
-return require(script.spring) :: types.springIniter
+	GetOffset: (self:spring)->number;
+	GetVelocity: (self:spring)->number;
+	GetAcceleration: (self:spring)->number;
+	GetGoal: (self:spring)->number;
 
+	SetExternalForce: (self:spring,force:number)->();
+	SetGoal: (self:spring,goal:number)->();
+	AddOffset: (self:spring,offset:number)->();
+	AddVelocity: (self:spring,velocity:number)->();
+	SetOffset: (self:spring,offset:number)->number;
+	InitResolver: (self:spring)->spring;
+}
+export type springIniter = {
+	New: (Mass:number,Damping:number,Constant:number,InitialOffset:number,InitialVelocity:number,ExternalForce:number)->spring
+}
+
+return require(script.spring) :: springIniter
