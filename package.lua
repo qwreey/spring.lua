af24fe7de6486f23cd583f54d9d80910dccce84f
Qwreey <qwreey75@gmail.com>
2023-12-07 20:38:37 +0900

    Create package.lua

---
+++ package.lua
@@ -0,0 +1,11 @@
+return {
+	name = 'qwreey/sping',
+	version = '1.0.0',
+	homepage = 'https://github.com/qwreey/spring.lua',
+	dependencies = {
+	},
+	tags = {'physics'},
+	license = 'MIT',
+	author = 'qwreey',
+	files = {'init.lua', 'main.lua'},
+}
